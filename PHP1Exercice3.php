4536739ebdc207702ab25b660c6ab79d077a112f
mika <migame.strass@gmail.com>
2023-02-02 16:29:54 +0100

    Création de lien hypertext vers un nouvel onglet

---
+++ PHP1Exercice3.php
@@ -0,0 +1,8 @@
+<h1> Exercice 3</h1>
+
+Afficher un lien hypertexte permettant d’accéder au site d’Elan Formation. Le lien devra s’ouvrir dans un nouvel onglet (_blank).
+
+<h2> Résultat</h2>
+<?php
+echo "<a href=
+'https://elan-formation.eu/planning'target='_blank'>ELAN Formation</a>";
