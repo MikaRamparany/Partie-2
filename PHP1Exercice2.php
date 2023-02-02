19d310e9b30e8b05cf9cf2336e239df00de89f31
mika <migame.strass@gmail.com>
2023-02-02 14:24:13 +0100

    création Table l.26

---
+++ PHP1Exercice2.php
@@ -0,0 +1,26 @@
+<h1> Exercice 2</h1>
+
+Soit le tableau suivant:
+$capitales= array ("France"=>"Paris","Allemagne"=>"Berlin","USA"=>"Washington","Italie"=>"Rome");
+<br>
+<br>
+
+Réaliser un algorithme permettant d’afficher le tableau HTML suivant (notez que le nom du pays s’affichera en majuscule et que le tableau est trié par ordre alphabétique du nom de pays) grâce à une fonction personnalisée.
+<br>
+<br>
+Vous devrez appeler la fonction comme suit: afficherTableHTML($capitales);
+<br>
+<br>
+<!-- Resume : 
+Nom pays => MAJ -> voir FONCTION "strtoupper"
+Tableau : ordre alphabétique algorithme.-> voir FONCTION "ksort" --> 
+
+<h2> Résultat </h2>
+
+
+
+<!-- VARIABLES-->
+
+<?php 
+
+echo "<table> 
