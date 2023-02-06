d610d7be656e06443c08f94aa1256ab468e8da0d
Mika <migame.strass@gmail.com>
2023-02-06 09:08:49 +0100

    Ajout autre formule à option 3, formule RETURN

--- PHP1Exercice10.php
+++ PHP1Exercice10.php
@@ -1,14 +1,15 @@
-<h1>Exercice 10 </h1> 
+<h1>Exercice 10 </h1>
 
-En utilisant l’ensemble des fonctions personnalisées crées précédemment,  créer  un  formulaire complet  qui  contient  les  informations  suivantes: <br>
+En utilisant l’ensemble des fonctions personnalisées crées précédemment, créer un formulaire complet qui contient les
+informations suivantes: <br>
 
-champs  de  texte avec : <br>  nom,  prénom,  adresse  e-mail, ville, sexe 
+champs de texte avec : <br> nom, prénom, adresse e-mail, ville, sexe
 
-<br> <br> 
-
-Et une liste de choix parmi lesquels on pourra sélectionner un intitulé de formation : 
 <br> <br>
-  «Développeur Logiciel»,«Designer web», «Intégrateur» ou «Chef de projet».
+
+Et une liste de choix parmi lesquels on pourra sélectionner un intitulé de formation :
+<br> <br>
+«Développeur Logiciel»,«Designer web», «Intégrateur» ou «Chef de projet».
 
 <br> <br>
 Le formulaire devra également comporter un bouton permettant de le soumettre à un traitement de validation (submit).
@@ -19,53 +20,75 @@ Le formulaire devra également comporter un bouton permettant de le soumettre à
 
 Informations personnelles : <br> <br>
 
-<?php 
+<?php
 
 $nomsInput = array("Nom", "Prénom", "Adresse e-mail", "Ville", "Sexe");
-function afficherInput($array){
+function afficherInput($array)
+{
     echo "<form style='background-color:E8E3E2;width: 180px; padding:6px'>"; // permet d'afficher la forme du tableau, couleur de fond, taille, padding.
-        foreach ($array as $val){
-            echo "<p>".$val."<br><input type='text'/></p>"; // input type : pour le formulaire en liste normal
-        }
+    foreach ($array as $val) {
+        echo "<p>" . $val . "<br><input type='text'/></p>"; // input type : pour le formulaire en liste normal
+    }
     echo "</form>";
 }
 afficherInput($nomsInput);
-?> 
+?>
 
 Quelle formation souhaitez vous choisir ? <br> <br>
 
-<form style='background-color:E8E3E2;width: 180px; padding:6px' method="POST"> 
+<form style='background-color:E8E3E2;width: 180px; padding:6px' method="POST">
     <input type="checkbox" name="mon_champ[]" value="Choix
-   1"/>Développeur Logiciel<br><br>
+   1" />Développeur Logiciel<br><br>
     <input type="checkbox" name="mon_champ[]" value="Choix
-   2"/>Designer web<br><br>
+   2" />Designer web<br><br>
     <input type="checkbox" name="mon_champ[]" value="Choix
-   3"/>Intégrateur» ou «Chef de projet<br><br>
+   3" />Intégrateur» ou «Chef de projet<br><br>
     <input type="submit" value="OK">
-   </form>
-   
+</form>
 
-<?php 
+
+<?php
 // method=POST (ligne 37) permet d'ajouter des cases à cocher 
 // inpu type = submit + value = OK : permet d'ajouter un boutton OK de validation du formulaire. 
 
 $string = "Option 2:  tableaux 3 :<br>";
 
-echo "<p style='color:red'>".$string;
+echo "<p style='color:red'>" . $string;
 
- // Tableau 3 comme autre option pour l'exercice 
+// Tableau 3 comme autre option pour l'exercice 
 
-$choixForma = array ("Développeur Logiciel","Designer web", "Intégrateur", "chef de projet");
-function alimenterListeDeroulante($array){
+$choixForma = array("Développeur Logiciel", "Designer web", "Intégrateur", "chef de projet");
+function alimenterListeDeroulante($array)
+{
     echo "<select name = 'civilité'> ";
-    foreach($array as $choixforma){
-       
-        echo"<option value = '$choixforma'> $choixforma 
+    foreach ($array as $choixforma) {
+
+        echo "<option value = '$choixforma'> $choixforma 
         </option>"; // OPTION VALUE permet de faire une liste déroulante. 
-        
+
         "</select>";
-        
+
     }
+    echo "<input type = 'submit' value = 'Ok'/>";
 }
-alimenterListeDeroulante($choixForma);
-echo "<input type = 'submit' value = 'Ok'/>"; 
+
+// Autre moyen pour afficher l'option 3 => AU LIEU D'UTILISER "ECHO" pour afficher la liste déroulante. Utiliser une fonction RETURN
+function alimenterListeDeroulanteReturn($array)
+{
+    $resultat = "<select name = 'civilité'> ";
+    foreach ($array as $choixforma) {
+
+        $resultat .= "<option value = '$choixforma'> $choixforma 
+        </option>"; // OPTION VALUE permet de faire une liste déroulante. 
+
+
+    }
+    $resultat .= "</select>";
+    $resultat .= "<input type = 'submit' value = 'Ok'/>";
+
+    return $resultat;
+}
+
+
+
+echo alimenterListeDeroulanteReturn($choixForma);
