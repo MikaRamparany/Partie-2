dbfd64be6fdb1e899abcb1e9519255c5b88010b2
Mika <migame.strass@gmail.com>
2023-02-06 11:47:54 +0100

    Début exercice 13, création class Voiture

---
+++ PHP1Exercice13.php
@@ -0,0 +1,22 @@
+<h1> Exercice 13</h1> 
+
+Créer  une  classe  Voiture  possédant  les  propriétés  suivantes: <br> <br>
+
+marque,  modèle,  nbPortes et vitesseActuelleainsi que les méthodes demarrer( ), accelerer( )et stopper( )en plus des  accesseurs  (get)  et  mutateurs  (set)  traditionnels.  <br><br>
+
+La  vitesse  initiale  de  chaque  véhicule instancié est de 0. <br>
+Une méthode personnalisée pourra afficher toutes les informations d’un véhicule. 
+
+<h2> Résultat </h2>
+
+<?php
+
+class Voiture{
+    private $_marque;
+    private $_modele;
+    private $_nbPortes;
+    private $_vitesseInitiale;
+        public function demarrer(){
+        $this>$_vitesseInitiale == 0;
+        }
+}
