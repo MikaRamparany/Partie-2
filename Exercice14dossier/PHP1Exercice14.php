240dc9ee197a5499778710bfab9c5b31a7a20d35
mika <migame.strass@gmail.com>
2023-02-08 16:55:24 +0100

    création page exercice 14

---
+++ Exercice14dossier/PHP1Exercice14.php
@@ -0,0 +1,20 @@
+<h1> Exercice 14</h1>
+
+Créer une classe Voiture possédant 2 propriétés(marque et modèle) ainsi qu’une classe VoitureElec qui hérite(extends) de la classe Voiture et qui a une propriété supplémentaire (autonomie).
+<br>
+
+<br>Instancier une voiture «classique» et une voiture «électrique» ayant les caractéristiques suivantes: <br>
+
+<br>Peugeot 408: $v1 = new Voiture("Peugeot","408");<br>
+<br>BMW i3150: $ve1 = new VoitureElec("BMW","I3",100);<br>
+
+<h2> Résultat</h2>
+
+<?php
+
+class Voiture
+{
+    private string $_marque;
+    private string $_modele;
+
+}
