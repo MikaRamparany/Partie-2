8ed8ca86f8f5cce7baa9a8c489aab8496d7783ad
mika <migame.strass@gmail.com>
2023-02-10 14:17:25 +0100

    Création valeur $email et  utilisation "if" /else -> function FILTER_Validate_Email // Lignes

---
+++ Partie-2/PHP1Exercice15.php
@@ -0,0 +1,18 @@
+<h1> Exercice 15 </h1>
+
+En  utilisant  les  ressources  de  la  page http://php.net/manual/fr/book.filter.php,  vérifier  si  une adresse e-mail a le bon format.
+
+
+
+<h2> Résultat </h2>
+
+<?php 
+
+$email = "elan@elan-formation.fr";
+// Valider l'email
+if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+  echo "L'adresse  <b>$email</b> est une adresse e-mail valide";
+}else{
+  echo "L'adresse e-mail n'est pas valide";
+}
+?>
